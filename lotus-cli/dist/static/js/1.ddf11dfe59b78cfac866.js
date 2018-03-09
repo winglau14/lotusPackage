000b43c1110b99934eab7c953eff6e077bd6c497
winglau14 <397069329@qq.com>
2018-03-10 00:35:34 +0800

    删除

--- lotus-cli/dist/static/js/1.ddf11dfe59b78cfac866.js
+++
@@ -1,2 +0,0 @@
-/*! hey,lotus-cli */
-webpackJsonp([1],{144:function(e,l,n){"use strict";Object.defineProperty(l,"__esModule",{value:!0});var u=n(8),t=u(null,null,!1,null,null,null);t.options.__file="src\\components\\Test.vue",l.default=t.exports}});
--- lotus-cli/src/Utils/lockBody.js
+++
@@ -1,9 +0,0 @@
-export default (heigthValue,overflowValue) =>{
-    if(heigthValue === 'auto' && overflowValue === 'auto'){
-        document.body.className = '';
-        document.documentElement.className = '';
-    }else{
-        document.body.className = 'lock-body';
-        document.documentElement.className = 'lock-body';
-    }
-}
