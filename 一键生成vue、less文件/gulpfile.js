913b0f31546717097e0cc2c10f5dee0a5f6c9004
winglau14 <397069329@qq.com>
2018-03-23 17:57:07 +0800

    新增判断文件夹or文件是否存在

--- 一键生成vue、less文件/gulpfile.js
+++ 一键生成vue、less文件/gulpfile.js
@@ -3,14 +3,14 @@ const path = require("path");
 const fs = require("fs");
 const inquirer = require('inquirer');
 const gulpSequence = require('gulp-sequence');
-let vuePageName = '';
+/*let vuePageName = '';
 let vueComponentsName = '';
 let lessFolderName = '';
-let lessFileName = '';
+let lessFileName = '';*/
 //vue组件初始化内容
-function vueComponentContent(lessFolderName, lessFileName) {
+function vueComponentContent(lessFolderName, lessFileName,lessType) {
     let lessStr = '';
-    if(lessFolderName){
+    if(lessFolderName && lessType === 'lessVuePage'){
         //vue页面引入less
         lessStr = `'../../style/${lessFolderName}/${lessFileName}.less';`;
     }else{
@@ -55,7 +55,6 @@ gulp.task("createFile", function () {
             type: 'list',
             name: 'vueType',
             message: '选择要生成的vue类型页面 or 组件 驼峰命名首字母小写',
-            // choices: ['less', 'scss', 'css'],
             choices: ['vuePage','vueComponent'],
             default: 'vuePage'
         },
@@ -85,7 +84,7 @@ gulp.task("createFile", function () {
         {
             type: 'input',
             name: 'lessFolderName',
-            message: 'less文件命名名称 驼峰命名首字母小写',
+            message: 'less文件夹命名名称 驼峰命名首字母小写',
             default: 'less'
         },
         //less文件命名名称
@@ -99,7 +98,7 @@ gulp.task("createFile", function () {
         {
             type: 'input',
             name: 'lessClassName',
-            message: 'less文件内父级className,超过两个用list-model',
+            message: 'less文件内父级className命名,超过两个用list-model',
             default: 'list'
         }
     ]).then(function (response) {
@@ -107,54 +106,116 @@ gulp.task("createFile", function () {
         for (let i in response) {
             if (i === 'vueType') {//生成对应vuePage文件夹
                 //生成vue页面文件夹
-                if(response['vueType'] === 'vuePage' && response['vuePageName']){
-                    fs.mkdir(`test/${response['vuePageName']}`);
+                if(response['vueType'] === 'vuePage'){
+                    fs.exists(`test/${response['vuePageName']}`,function(exists){
+                        if(exists){
+                            console.log('文件夹已存在，创建失败！');
+                            //写入文件 vue页面
+                            fs.writeFile(`test/${response['vuePageName']}/${response['vueComponentName']}.vue`, vueComponentContent(response['lessFolderName'], response['lessFileName'], response['lessType']), 'utf-8',function(){
+                                console.log('vuePage 写入成功');
+                                readRouter(response['vuePageName'],response['vueComponentName'],response['vueType']);
+                            });
+                            //写入less文件
+                            if(response['lessType'] === 'lessVuePage'){
+                                //判断less文件夹是否存在
+                                fs.exists(`test/style/${response['lessFolderName']}`,function(exists){
+                                    if(exists){
+                                        console.log('vuePage less文件存在，创建失败！');
+                                        console.log('vuePage less文件创建成功！');
+                                        //写入less 文件
+                                        const lessClassName = response['lessClassName'];
+                                        fs.writeFile(`test/style/${response['lessFolderName']}/${response['lessFileName']}.less`, lessContent(lessClassName), 'utf-8');
+                                    }else{
+                                        fs.mkdir(`test/style/${response['lessFolderName']}`,function(err){
+                                            if (err) {
+                                                return err;
+                                            }
+                                            console.log('vuePage less文件创建成功！');
+                                            //写入less 文件
+                                            const lessClassName = response['lessClassName'];
+                                            fs.writeFile(`test/style/${response['lessFolderName']}/${response['lessFileName']}.less`, lessContent(lessClassName), 'utf-8');
+                                        });
+                                    }
+                                });
+                            }
+                        }else{
+                            fs.mkdir(`test/${response['vuePageName']}`,function(err){
+                                if (err) {
+                                    return err;
+                                }
+                                console.log('vuePage 创建成功！');
+                                //写入文件 vue页面
+                                fs.writeFile(`test/${response['vuePageName']}/${response['vueComponentName']}.vue`, vueComponentContent(response['lessFolderName'], response['lessFileName'], response['lessType']), 'utf-8',function(){
+                                    console.log('vuePage 写入成功');
+                                    readRouter(response['vuePageName'],response['vueComponentName'],response['vueType']);
+                                });
+                                //写入less文件
+                                if(response['lessType'] === 'lessVuePage'){
+                                    //判断less文件夹是否存在
+                                    fs.exists(`test/style/${response['lessFolderName']}`,function(exists){
+                                        if(exists){
+                                            console.log('vuePage less文件存在，创建失败！');
+                                            //写入less 文件
+                                            const lessClassName = response['lessClassName'];
+                                            fs.writeFile(`test/style/${response['lessFolderName']}/${response['lessFileName']}.less`, lessContent(lessClassName), 'utf-8');
+                                        }else{
+                                            fs.mkdir(`test/style/${response['lessFolderName']}`,function(err){
+                                                if (err) {
+                                                    return err;
+                                                }
+                                                console.log('vuePage less文件创建成功！');
+                                                //写入less 文件
+                                                const lessClassName = response['lessClassName'];
+                                                fs.writeFile(`test/style/${response['lessFolderName']}/${response['lessFileName']}.less`, lessContent(lessClassName), 'utf-8');
+                                            });
+                                        }
+                                    });
+                                }
+                            });
+                        }
+                    });
                 }else{
                     //生成vue组件文件夹
-                    fs.mkdir(`test/components/${response['vuePageName']}`);
-                }
-            } else if (i === 'vueComponentName') {//生成对应vuePage文件夹
-                //拼接vuePage文件里面less文件引入路径
-                vuePageName = response['vuePageName'];
-                vueComponentsName = response['vueComponentName'];
-                if(response['vueType'] === 'vuePage'){
-                    lessFolderName = response['lessFolderName'];
-                }
-                lessFileName = response['lessFileName'];
-                if(response['vueType'] === 'vuePage'){
-                    //写入文件 vue页面
-                    fs.writeFile(`test/${response['vuePageName']}/${response['vueComponentName']}.vue`, vueComponentContent(lessFolderName, lessFileName), 'utf-8',function(){
-                        readRouter(vuePageName,vueComponentsName,response['vueType']);
+                    fs.exists(`test/${response['vuePageName']}`,function(exists){
+                        if(exists){
+                            console.log('vueComponent 已存在，创建失败！');
+                            console.log('vueComponent 创建成功！');
+                            const lessFolderName = response['lessFolderName'];
+                            const lessFileName = response['lessFileName'];
+                            //写入文件 vue组件
+                            fs.writeFile(`test/components/${response['vuePageName']}/${response['vueComponentName']}.vue`, vueComponentContent(lessFolderName, lessFileName,response['lessType']), 'utf-8',function(){
+                                console.log('vueComponent 写入成功');
+                                //写入less 文件
+                                const lessClassName = response['lessClassName'];
+                                fs.writeFile(`test/components/${response['lessFolderName']}/${response['lessFileName']}.less`, lessContent(lessClassName), 'utf-8');
+                            });
+                        }else{
+                            fs.mkdir(`test/components/${response['vuePageName']}`,function(err){
+                                if (err) {
+                                    return err;
+                                }
+                                console.log('vueComponent 创建成功！');
+                                const lessFolderName = response['lessFolderName'];
+                                const lessFileName = response['lessFileName'];
+                                //写入文件 vue组件
+                                fs.writeFile(`test/components/${response['vuePageName']}/${response['vueComponentName']}.vue`, vueComponentContent(lessFolderName, lessFileName,response['lessType']), 'utf-8',function(){
+                                    console.log('vueComponent 写入成功');
+                                    //写入less 文件
+                                    const lessClassName = response['lessClassName'];
+                                    fs.writeFile(`test/components/${response['lessFolderName']}/${response['lessFileName']}.less`, lessContent(lessClassName), 'utf-8');
+                                });
+                            });
+                        }
                     });
-                }else{
-                    //写入文件 vue组件
-                    fs.writeFile(`test/components/${response['vuePageName']}/${response['vueComponentName']}.vue`, vueComponentContent(lessFolderName, lessFileName), 'utf-8',function(){
-                        readRouter(vuePageName,vueComponentsName,response['vueType']);
-                    });
                 }
-
-            } else if (i === 'lessType') {
-                //生成less文件夹
-                //生成vue页面文件夹
-                if(response['lessType'] === 'lessVuePage'){
-                    fs.mkdir(`test/style/${response['lessFolderName']}`);
-                }
-            } else if (i === 'lessFileName') {
-                const lessClassName = response['lessClassName'];
-                //生成vue页面文件夹 生成less文件
-                if(response['lessType'] === 'lessVuePage'){
-                    //写入文件
-                    fs.writeFile(`test/style/${response['lessFolderName']}/${response['lessFileName']}.less`, lessContent(lessClassName), 'utf-8');
-                }else{
-                    //写入文件
-                    fs.writeFile(`test/components/${response['lessFolderName']}/${response['lessFileName']}.less`, lessContent(lessClassName), 'utf-8');
-                }
+                return false;
             }
         }
     })
 });
 //读取router.js文件
 function readRouter(vuePageName, vueComponentsName,type) {
+    console.log(vuePageName, vueComponentsName);
     if(type === "vueComponent"){
         return false;
     }
