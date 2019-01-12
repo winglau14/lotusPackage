bb63c9fa247d6908aedc5718ba75f911e06993a0
winglau14 <397069329@qq.com>
2019-01-12 14:56:50 +0800

    代码更新

---
+++ pc-es62es5/src/static/js/index.js
@@ -0,0 +1,20 @@
+import {lotusPagination} from "../../static/js/lib/lotusPagination";
+import {utils} from "../../static/js/utils";
+import "../../static/style/xyzCommon.css"
+lotusPagination.init({
+    index: 0, //翻页索引值
+    totalNum: 1719, //总数量
+    showPageSize: 16, //一页显示多少个
+    maxShowNum: 7, //分页最大显示数量
+    parentDom: "lotus-pagination",
+    leftPageTotal: "lotus-pagination-total-num",
+    domItemList: "lotus-pagination-list-item",
+    prevBtn: "lotus-pagination-list-prev",
+    nextBtn: "lotus-pagination-list-next",
+    enSureBtn: "lotus-pagination-list-ensure",
+    curNum: "lotus-pagination-list-cur",
+    rightPageTotal: "lotus-pagination-list-total",
+    //hideLeftPageTotal:true,
+    callback: function (res) {
+    }
+});
